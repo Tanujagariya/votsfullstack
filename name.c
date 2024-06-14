5d1a5b6edf1ac8e236f188c3acbbd5f525f51137
SURAJ SINGH <rsingh852126@gmail.com>
2024-06-14 11:33:31 +0530

    name

---
+++ name.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int number;
+    printf("enter the number");
+    for (int i = 0; i < 2; i++)
+    {
+        scanf("%d",number);
+    }
+    for (int i = 0; i < 3; i++)
+    {
+        printf("%d",number);
+    }
+    return 0;
+    
+    
+}
