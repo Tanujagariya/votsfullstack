24cf81514f3e617de6f833e4d1a3690eb0804dab
SURAJ SINGH <rsingh852126@gmail.com>
2024-06-15 10:15:03 +0530

    nameprint

---
+++ nameprint.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int i;
+    char x[100];
+    printf("enter the name");
+    scanf("%s\t",x);
+    for ( i =1; i <=100; i++)
+    {
+        printf("%s\t",x);
+    }
+
+    return 0;
+    
+    
+}
