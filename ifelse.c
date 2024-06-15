7799c2437ee447c7b1451ed883df483e5426d7ca
SURAJ SINGH <rsingh852126@gmail.com>
2024-06-15 10:02:39 +0530

    ifelse

---
+++ ifelse.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+  for (int  i = 0; i <=30; i++)
+  {
+    if (i==5 || i==18)
+    {
+        
+    }
+    else
+    {
+   printf("%d\n",i);
+    }
+  }
+  return 0;
+    
+}
