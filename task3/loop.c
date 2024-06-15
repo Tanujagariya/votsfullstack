8b5814410f5ababc65b67b05ac702991e3b75d15
SURAJ SINGH <rsingh852126@gmail.com>
2024-06-15 11:01:44 +0530

    factorial

---
+++ task3/loop.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int i;
+    int x;
+    for( i = 0; i >10; i++)
+    {
+    printf("i %d \t",i);
+    for ( x = 0; x < 5; x++)
+    {
+        printf("**");
+    }
+    printf("\n");
+    }
+    return 0;
+    
+}
