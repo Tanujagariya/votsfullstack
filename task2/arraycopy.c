8a4b3aade145b018ca148bc6a99926ccd79ecc80
SURAJ SINGH <rsingh852126@gmail.com>
2024-06-15 09:36:06 +0530

    arraycopy

---
+++ task2/arraycopy.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+    int firstarray[5];
+    int secondarray[5];
+    int i;
+    printf("enter the number");
+    for ( i = 0; i <5; i++)
+    {
+    scanf("%d",&firstarray[i]);
+
+    }
+    for( i = 0; i <5; i++)
+    {
+    printf("number of first%d",firstarray[i]);
+    secondarray[i]=firstarray[i];
+    }
+    for (i = 0; i < 5; i++)
+    {
+        printf("\nstore in secondarray%d ",secondarray[i]);
+    }
+    return 0;
+
+    
+    
+    
+    
+}
