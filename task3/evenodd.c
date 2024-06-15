df3d16c7b2e75696bd656bd2293ac1218cac63ba
SURAJ SINGH <rsingh852126@gmail.com>
2024-06-15 10:33:12 +0530

    evenodd

---
+++ task3/evenodd.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int number;
+    printf("enter the number");
+    scanf("%d",&number);
+    if (number%2==0)
+
+    {
+        printf("this is a even number");
+
+    }
+    else
+    {
+        printf("this is an odd number%d",number);
+    }
+    return 0;
+    
+
+}
